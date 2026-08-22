66d3206dc21d96c664ec315c061454b86e782bd6
Jarosław Pelczar <jarek@jpelczar.com>
2026-08-22 08:26:55 +0200

    Bump project version to 1.0.3

--- git_cp_pr.py
+++ git_cp_pr.py
@@ -7,7 +7,7 @@ import sys
 import time
 from typing import List, Dict, Tuple
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 class Color:
     # Use ANSI codes, checking if terminal supports color (enabled by default)
